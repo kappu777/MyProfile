b14460f40ab370f10587f050e20d8b8add3962f3
Kapil Singhal <kapsingh5@publicisgroupe.net>
2025-09-19 23:54:37 -0700

    update url

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'Kapil Singhal',
     description:
       'Kapil Singhal software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://kappu777.github.io/portfolio', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@kapil777',
   },
